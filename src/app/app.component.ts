dc49ef5bc972e21c73fb639db6678de0acc2488c
victoriaemanuelle <vickmanu9@gmail.com>
2024-06-03 12:10:01 -0300

    Angular registro de funcionario

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 import { MatIconModule } from '@angular/material/icon';
@@ -7,25 +7,110 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { NgFor } from '@angular/common';
 import { EmpAddEditComponent } from './emp-add-edit/emp-add-edit.component';
+import { EmployeeService } from './services/employee.service';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatLabel, MatFormField } from '@angular/material/form-field';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import { AfterViewInit } from '@angular/core';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, MatIconModule, MatButtonModule, MatToolbarModule,
-    MatDialogModule
+    MatDialogModule, MatTableModule, MatLabel, MatFormField,
+    MatPaginatorModule, MatSortModule, MatInputModule, MatFormFieldModule
   ],
+  providers: [EmployeeService],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements AfterViewInit {
+  displayedColumns: string[] = [
+    'id',
+    'firstName',
+    'lastName',
+    'email',
+    'dob',
+    'gender',
+    'education',
+    'company',
+    'experience',
+    'package',
+    'edit',
+    'delete'
+  ];
+
   title = 'Crudemp';
 
-  constructor(private _dialog: MatDialog) { }
+  dataSource!: MatTableDataSource<any>;
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+
+
+  constructor(private _dialog: MatDialog,
+    private _empService: EmployeeService
+  ) { }
+  ngAfterViewInit(): void {
+  }
+
   ngOnInit(): void {
+    this.getEmployeeList();
   }
 
   openAddEditEmpForm() {
-    this._dialog.open(EmpAddEditComponent)
+    const dialogRef = this._dialog.open(EmpAddEditComponent)
+    dialogRef.afterClosed().subscribe({
+      next: (val) => {
+        if (val) {
+          this.getEmployeeList();
+        }
+      }
+    })
+  }
+
+  getEmployeeList() {
+    this._empService.getEmployeeList().subscribe({
+      next: (res) => {
+        this.dataSource = new MatTableDataSource(res);
+        this.dataSource.sort = this.sort;
+        this.dataSource.paginator = this.paginator;
+      },
+      error:
+        console.log
+
+    })
+  }
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+
+  deleteEmployee(id: any) {
+    this._empService.deleteEmployee(id).subscribe({
+      next: (res) => {
+        alert('Employee deleted!')
+      },
+      error: console.log,
+    })
+  }
+
+  openEditForm(data: any) {
+    this._dialog.open(EmpAddEditComponent, {
+      data,
+    })
   }
 }
+
+
--- src/app/emp-add-edit/emp-add-edit.component.ts
+++ src/app/emp-add-edit/emp-add-edit.component.ts
@@ -1,23 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatDatepickerModule } from '@angular/material/datepicker';
-import { provideNativeDateAdapter } from '@angular/material/core';
 import { MatRadioModule } from '@angular/material/radio';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSelectModule } from '@angular/material/select';
 import { NgFor } from '@angular/common';
 import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
-
+import { DialogRef } from '@angular/cdk/dialog';
+import { EmployeeService } from '../services/employee.service';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { MatDialogClose } from '@angular/material/dialog';
+import { provideNativeDateAdapter } from '@angular/material/core';
 
 
 @Component({
   selector: 'app-emp-add-edit',
   standalone: true,
-  providers: [provideNativeDateAdapter()],
+  providers: [provideNativeDateAdapter(), EmployeeService],
   imports: [MatInputModule, MatFormFieldModule, FormsModule,
     MatDatepickerModule, MatRadioModule, MatButtonModule,
-    ReactiveFormsModule, NgFor, MatSelectModule
+    ReactiveFormsModule, NgFor, MatSelectModule, MatDialogClose
   ],
   templateUrl: './emp-add-edit.component.html',
   styleUrl: './emp-add-edit.component.css'
@@ -33,8 +36,14 @@ export class EmpAddEditComponent implements OnInit {
     ];
 
   empForm: FormGroup;
+  Save: any;
 
-  constructor(private _fb: FormBuilder) {
+
+  constructor(private _fb: FormBuilder,
+    private _dialogRef: MatDialogRef<EmpAddEditComponent>,
+    private _empService: EmployeeService,
+    @Inject(MAT_DIALOG_DATA) public data: any
+  ) {
     this.empForm = this._fb.group({
       firstName: '',
       lastName: '',
@@ -47,12 +56,34 @@ export class EmpAddEditComponent implements OnInit {
       package: ''
     });
   }
-  onFormSubmit() {
-    if (this.empForm.valid) {
-      console.log(this.empForm.value);
-    }
+  ngOnInit(): void {
+    this.empForm.patchValue(this.data)
   }
 
-  ngOnInit(): void {
+  onFormSubmit() {
+    if (this.empForm.valid) {
+      if (this.data) {
+        this._empService.updateEmployee(this.data.id, this.empForm.value)
+          .subscribe({
+            next: (val: any) => {
+              alert('Employee updated sucessfully');
+              this._dialogRef.close(true);
+            },
+            error: (err: any) => {
+              console.error(err)
+            }
+          })
+      } else {
+        this._empService.addEmployee(this.empForm.value).subscribe({
+          next: (val: any) => {
+            alert('Employee added sucessfully');
+            this._dialogRef.close(true);
+          },
+          error: (err: any) => {
+            console.error(err)
+          },
+        });
+      }
+    }
   }
 }
